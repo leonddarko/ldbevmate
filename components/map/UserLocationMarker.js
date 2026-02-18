1dd86d3931d0b46d085798b4f97a2ca25bb68f8d
leonddarko <leonddarko@gmail.com>
2026-02-18 17:38:44 +0000

    Pages update

--- components/map/UserLocationMarker.js
+++ components/map/UserLocationMarker.js
@@ -7,8 +7,8 @@ const userIcon = new L.DivIcon({
   className: "",
   html: `
     <div class="relative">
-      <span class="absolute inline-flex h-6 w-6 rounded-full bg-blue-400 opacity-75 animate-ping"></span>
-      <span class="relative inline-flex rounded-full h-6 w-6 bg-blue-600 border-2 border-white"></span>
+      <span class="absolute inline-flex h-4 w-4 rounded-full bg-blue-400 opacity-75 animate-ping"></span>
+      <span class="relative inline-flex rounded-full h-4 w-4 bg-blue-600 border-2 border-white"></span>
     </div>
   `,
   iconSize: [20, 20],
