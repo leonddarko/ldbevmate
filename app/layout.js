0c37f3d84823bade71afb3c4b8e213851e1b37af
leonddarko <leonddarko@gmail.com>
2026-03-16 16:45:46 +0000

    Leaflet Map update

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
-import "mapbox-gl/dist/mapbox-gl.css";
 
 import { getServerSession } from "next-auth";
 import SessionProvider from "@/lib/SessionProvider";
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-const MapView = dynamic(() => import("@/components/map/MapboxMapContainer"), {
+const MapView = dynamic(() => import("@/components/map/MapContainer"), {
   ssr: false,
 });
 
